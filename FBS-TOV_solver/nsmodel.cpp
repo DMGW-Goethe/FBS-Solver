a76d91a27f9b2119901cabc322cdafc99a08ca0f
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2022-04-26 17:51:05 +0200

    separated class and base

--- FBS-TOV_solver/nsmodel.cpp
+++ FBS-TOV_solver/nsmodel.cpp
@@ -1,7 +1,11 @@
 #include "nsmodel.hpp"
 
+vector NSmodel::dy_dt_static(const double r, const vector& y, const void* params) {
+    NSmodel* m = (NSmodel*)params;
+    return m->dy_dt(r, y);
+}
 
-vector NSmodel::dy_dt(const double r, const vector& vars) {
+vector FermionBosonStar::dy_dt(const double r, const vector& vars) {
 
     // rename input & class variables for simpler use:
     const double a = vars[0]; const double alpha = vars[1]; const double Phi = vars[2]; const double Psi = vars[3]; double P = vars[4];
@@ -18,27 +22,23 @@ vector NSmodel::dy_dt(const double r, const vector& vars) {
     // apply the EOS:
     myEOS.callEOS(rho, epsilon, P); // change rho and epsilon by pointer using EOS member function
 
-    if( std::isnan(vars[0]) || std::isnan(vars[1]) || std::isnan(vars[2]) || std::isnan(vars[3]) || std::isnan(vars[4])) {
-        std::cout << "Nan found" << std::endl;
-        exit(1);}
+    if( vector::is_nan(vars)) {
+        std::cout << "Nan found: " << vars << std::endl;
+        assert(false);}
 
     // compute the ODEs:
-    double da_dr = 0.5* a * ( (1.-a*a) / r + 4.*M_PI*r*( (omega*omega/ alpha/alpha + mu*mu + 0.5*lambda*Phi*Phi )*a*a*Phi*Phi + Psi*Psi + 2.*a*a*rho*(1.+epsilon) ) ); // a (g_rr component) ODE
-    double dalpha_dr = 0.5* alpha * ( (a*a-1.) / r + 4.*M_PI*r*( (omega*omega/ alpha/alpha - mu*mu - 0.5*lambda*Phi*Phi )*a*a*Phi*Phi + Psi*Psi + 2.*a*a*P ) ); // alpha (g_tt component) ODE
-    double dPhi_dr = Psi; // Phi ODE
-    double dPsi_dr = -( 1. + a*a - 4.*M_PI*r*r*a*a*( mu*mu*Phi*Phi + 0.5*lambda*Phi*Phi*Phi*Phi + rho*(1.+epsilon) - P ))*Psi/r - (omega*omega/ alpha/alpha - mu*mu - lambda*Phi*Phi )*a*a*Phi; // Psi ODE
-    double dPres_dr = -(rho*(1.+epsilon) + P)*dalpha_dr/alpha; // Pressure ODE
+    double da_dr = 0.5* a * ( (1.-a*a) / r + 4.*M_PI*r*( (omega*omega/ alpha/alpha + mu*mu + 0.5*lambda*Phi*Phi )*a*a*Phi*Phi + Psi*Psi + 2.*a*a*rho*(1.+epsilon) ) );
+    double dalpha_dr = 0.5* alpha * ( (a*a-1.) / r + 4.*M_PI*r*( (omega*omega/ alpha/alpha - mu*mu - 0.5*lambda*Phi*Phi )*a*a*Phi*Phi + Psi*Psi + 2.*a*a*P ) );
+    double dPhi_dr = Psi;
+    double dPsi_dr = -( 1. + a*a - 4.*M_PI*r*r*a*a*( mu*mu*Phi*Phi + 0.5*lambda*Phi*Phi*Phi*Phi + rho*(1.+epsilon) - P ))*Psi/r - (omega*omega/ alpha/alpha - mu*mu - lambda*Phi*Phi )*a*a*Phi;
+    double dP_dr = -(rho*(1.+epsilon) + P)*dalpha_dr/alpha;
 
     // write the ODE values into output vector:
-    return vector({da_dr, dalpha_dr, dPhi_dr, dPsi_dr, dPres_dr});
+    return vector({da_dr, dalpha_dr, dPhi_dr, dPsi_dr, dP_dr});
 }
 
-vector NSmodel::dy_dt_static(const double r, const vector& y, const void* params) {
-    NSmodel* m = (NSmodel*)params;
-    return m->dy_dt(r, y);
-}
 
-vector NSmodel::initial_conditions(const double r0, const double rho_0, const double phi_0) {
+vector FermionBosonStar::initial_conditions(const double r0, const double rho_0, const double phi_0) {
         return vector( {1.0, 1.0, phi_0, 0., this->EOS->get_P_from_rho(rho_0)});
 }
 
--- FBS-TOV_solver/nsmodel.hpp
+++ FBS-TOV_solver/nsmodel.hpp
@@ -5,20 +5,32 @@
 #include "eos.hpp"
 
 
-
+/* this is an abstract class that is supposed to be the backbone for
+ * a physical model of a neutron star
+ * */
 class NSmodel {
-
 public:
-    double mu, lambda, omega;
     std::shared_ptr<EquationOfState> EOS;
 
-    NSmodel(std::shared_ptr<EquationOfState> EOS, double mu, double lambda, double omega) : mu(mu),lambda(lambda), omega(omega), EOS(EOS) {}
+    NSmodel(std::shared_ptr<EquationOfState> EOS) : EOS(EOS) {}
+    virtual vector dy_dt(const double r, const vector& vars) =0;
+    static vector dy_dt_static(const double r, const vector& y, const void* params); // this is a static function that receive the class pointer in params and calls dy_dt
+};
+
+
+/* this is a class modeling a fermion boson star, akin to
+ * https://arxiv.org/pdf/2110.11997.pdf
+ * */
+class FermionBosonStar : public NSmodel {
+public:
+    double mu, lambda, omega;
+
+    FermionBosonStar(std::shared_ptr<EquationOfState> EOS, double mu, double lambda, double omega) : NSmodel(EOS), mu(mu),lambda(lambda), omega(omega) {}
 
     vector dy_dt(const double r, const vector& vars) ;
-    static vector dy_dt_static(const double r, const vector& y, const void* params); // this is a static function that receive the class pointer in params and calls dy_dt
     vector initial_conditions(const double r0, const double rho_0, const double phi_0) ;
-
 };
+
 
 
 
