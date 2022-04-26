bbe1e450aa9a080c7c75f768655a7735103bd4bf
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2022-04-26 16:40:12 +0200

    wrote bisection

--- FBS-TOV_solver/main.cpp
+++ FBS-TOV_solver/main.cpp
@@ -33,107 +33,113 @@ void save_integration_data(const std::vector<integrator::step>& res, std::string
 	img.close();
 }
 
-// integrating the system of equations using the shooting method:
-// for shoooting method see e.g. https://sdsawtelle.github.io/blog/output/shooting-method-ode-numerical-solution.html
-// parameters: star Radius, Mass, initial values, mu, lambda
-// returns only the Mass and fermionic radius of ONE star
-void Shooting_integrator_nosave_intermediate(double& Rfermionic, double& Mtot, vector& init_vars, NSmodel* m) {
 
-    // define the evolution variables
-    double omega_bar = 1.0;             // initial guess for omega and running variable
-    double omega_0bestguess; // this will be the best guess for omega
-    double delta_omega = 1e-3;           // to track the quality of the derivative for finite-difference
-    const double delta_omega_max = 100.;
-    const double root_desired_error = 1e-8;    // desired accuracy for the root F(omega)=0
+void Bisection(NSmodel*m, const vector& init_vars, double omega_0, double omega_1) {
+    // values for bisection
+    double omega_mid;
+    int n_zc_0, n_zc_1, n_zc_mid;
+    int n_mode = 0;
+    double delta_omega = 1e-16;
+    int phi_index = 2;
+    int n_max_steps = 1000;
+    int i = 0;
 
-    const double init_r = 1e-10;        // initial radius (cannot be zero due to coordinate singularity)
-    const double r_end = 1000.;
-    int max_step = 1000000;
+    // variables regarding the integration
+    integrator::IntegrationOptions intOpts;
+    double r_init = 1e-10, r_end= 1000;
+    integrator::Event phi_neg([](const double r, const vector y, const void*params) { return y[2] < 0.; });
+    integrator::Event phi_pos([](const double r, const vector y, const void*params) { return y[2] > 0.; });
+    std::vector<integrator::Event> events = {phi_neg, phi_pos};
+    std::vector<integrator::step> results_0, results_1, results_mid;
 
-    // declare the variables to be able to use them in the whole function scope
-    double my_r = init_r;
-    vector first_result, second_result;
-    double first_shot, second_shot;
+    // find initial values
+    assert(omega_0 < omega_1);
+    m->omega = omega_0;
+    int res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_0,  events, intOpts);
+    n_zc_0 = events[0].steps.size() + events[1].steps.size() -1;
 
-    integrator::Event Rf_condition([](const double r, const vector y, const void*params) { return y[4] < 1e-14; });
-    integrator::Event phi_condition([](const double r, const vector y, const void*params) { return y[2] < 1e-14; });
-    integrator::Event Rf_phi_condition([](const double r, const vector y, const void*params) { return std::max(y[2], y[4]) < 1e-14; }, true);;
-    std::vector<integrator::Event> events = {Rf_condition, phi_condition, Rf_phi_condition};
-    std::vector<integrator::step> results;
+    m->omega = omega_1;
+    res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_1,  events, intOpts);
+    n_zc_1 = events[0].steps.size() + events[1].steps.size() - 1;
+    assert(omega_0 < omega_1);
+    assert(n_zc_0 != n_zc_1);
+    assert(n_zc_0 <= n_mode && n_mode <= n_zc_1);
+    std::cout << "start with omega_0 =" << omega_0 << " with n_zc=" << n_zc_0 << " and omega_1=" << omega_1 << " with n_zc=" << n_zc_1 << std::endl;
 
-    // integrate one TOV star until a stable configuration has been reached (including shooting method):
-    // outer shooting method loop:
-    while(true) {
+    // find right number of zero crossings
+    while(n_zc_1 - n_zc_0 > 1) {
+        omega_mid = (omega_0 + omega_1)/2.;
+        std::cout << "omega_mid = " << omega_mid << " ->";
+        m->omega = omega_mid;
+        res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_mid,  events, intOpts);
+        n_zc_mid = events[0].steps.size() + events[1].steps.size() -1;
+        std::cout << " with n_zc = " << n_zc_mid << std::endl;
 
-        // reset the initial values before each ODE itegration (only omega should change each iteration):
-        Rfermionic = 0.0;
-        Mtot = 0.0;
-
-        // inner ODE integration loop first solution:
-        m->omega = omega_bar;
-        int res =  integrator::RKF45(&(m->dy_dt_static), init_r, init_vars, r_end, (void*) m, max_step,  results,  events);
-        first_result = results[results.size()-1].second; my_r = results[results.size()-1].first;
-
-        Rfermionic = events[0].steps.at(0).first;
-        std::cout << "stopped with events[0].active=" << events[0].active << ", events[1].active=" << events[1].active << ", events[2].active=" << events[2].active<< std::endl;
-        for(auto it = events.begin(); it != events.end(); ++it)
-            it->reset();
-
-        // second solution
-        m->omega = omega_bar + delta_omega;
-        res =  integrator::RKF45(&(m->dy_dt_static), init_r, init_vars, r_end, (void*) m, max_step,  results,  events, true);
-        for(auto it = events.begin(); it != events.end(); ++it)
-            it->reset();
-        second_result = results[results.size()-1].second;
-
-        first_shot = first_result[3];
-        second_shot = second_result[3];
-
-        // take result from the integrator and try to obtain a new value for omega:
-        // Newton-Raphson method to step in the right direction for omega
-        std::cout << "first result = " << first_result <<  "second result = " << second_result << std::endl;
-
-        if ( std::abs( first_shot ) < root_desired_error) { // stop the loop once a satisfactory level of accuracy has been found
-            break;
-		}
-        if ( first_shot == second_shot) {
-            std::cout << "omega = " << omega_bar << "no change detected. increase delta_omega=" << delta_omega << std::endl;
-            delta_omega *= 10.;
-            if(delta_omega > delta_omega_max)
-                break;
+        if(n_zc_mid == n_zc_0 || n_zc_mid <= n_mode) {
+            n_zc_0 = n_zc_mid;
+            omega_0 = omega_mid;
             continue;
         }
-        omega_0bestguess =  omega_bar - first_shot * delta_omega / ( second_shot - first_shot );
-		omega_bar = omega_0bestguess; // let the best guess become the starting point for the next iteration step
+        if(n_zc_mid == n_zc_1 || n_zc_mid >= n_mode) {
+            n_zc_1 = n_zc_mid;
+            omega_1 = omega_mid;
+            continue;
+        }
+    }
+    std::cout << "found omega_0 =" << omega_0 << " with n_zc=" << n_zc_0 << " and omega_1=" << omega_1 << " with n_zc=" << n_zc_1 << std::endl;
 
-        std::cout << "one_omega_iteration done! omega = " << omega_0bestguess << std::endl;
+    // find right behavior at infty
+    int n_inft_0, n_inft_1, n_inft_mid;
+    m->omega = omega_0; intOpts.save_intermediate=true;
+    res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_0,  events, intOpts);
+    n_inft_0 = results_0[results_0.size()-1].second[phi_index] > 0.;
+
+    m->omega = omega_1;
+    res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_1,  events, intOpts);
+    n_inft_1 = results_1[results_1.size()-1].second[phi_index] > 0.;
+    std::cout << "start with omega_0 =" << omega_0 << " with n_inft=" << n_inft_0 << " and omega_1=" << omega_1 << " with n_inft=" << n_inft_1 << std::endl;
+
+    plotting::plot_evolution(results_0, events, {2}, { "Phi_0" });
+    plotting::plot_evolution(results_1, events, {2}, { "Phi_1"});
+    matplotlibcpp::legend();
+    matplotlibcpp::save("bisection_int.png"); matplotlibcpp::close();
+
+    intOpts.save_intermediate=false;
+    while(omega_1 - omega_0 > delta_omega && i < n_max_steps) {
+        omega_mid = (omega_0 + omega_1)/2.;
+        std::cout << "omega_mid = " << omega_mid << " ->";
+        m->omega = omega_mid;
+        res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_mid,  events, intOpts);
+        n_inft_mid = results_mid[results_mid.size()-1].second[phi_index] > 0.;
+        std::cout << " with n_inft= " << n_inft_mid << std::endl;
+
+        i++;
+        if(n_inft_mid == n_inft_0) {
+            n_inft_0 = n_inft_mid;
+            omega_0 = omega_mid;
+            continue;
+        }
+        if(n_inft_mid == n_inft_1) {
+            n_inft_1 = n_inft_mid;
+            omega_1 = omega_mid;
+            continue;
+        }
     }
 
-    m->omega = omega_0bestguess;
-    plotting::plot_evolution(results, events, {0,1,2,3,4}, {"a", "alpha", "Phi", "Psi", "P"}, "test.png");
-    //save_integration_data(results, "test");
+    std::cout << "found omega_0 =" << omega_0 << " with n_inft=" << n_inft_0 << " and omega_1=" << omega_1 << " with n_inft=" << n_inft_1 << std::endl;
+    m->omega = omega_0;
 
-    Mtot = 0.5 * my_r * ( 1.0 - 1.0/(std::pow(first_result[0],2) ));
-}
+    m->omega = omega_0; intOpts.save_intermediate=true;
+    res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_0,  events, intOpts);
+
+    m->omega = omega_1;
+    res =  integrator::RKF45(&(m->dy_dt_static), r_init, init_vars, r_end, (void*) m,  results_1,  events, intOpts);
+    plotting::plot_evolution(results_0, events, {2}, {"Phi_0"});
+    plotting::plot_evolution(results_1, events, {2}, {"Phi_1"});
+    matplotlibcpp::legend();
+    matplotlibcpp::save("bisection_fin.png"); matplotlibcpp::close();
 
 
-// saves the MR-values in a text file
-template <typename Mat2D>
-void save_data_MR(Mat2D& array, const unsigned length, std::string filename) {
-
-	std::ofstream img;
-	img.open(filename);
-
-	if(img.is_open()) {
-
-		img << "# R [km]\t M [M_sun]\t rho_c [code units]" << std::endl;
-
-		for(unsigned i = 0; i < length; ++i) {
-
-			img <<std::fixed<<std::setprecision(10)<< array[i][0]*1.476625061 << " " << array[i][1] << " " << array[i][2] << std::endl;
-		}
-	}
-	img.close();
 }
 
 
@@ -144,16 +150,12 @@ int main() {
     //matplotlibcpp::backend("TkAgg");
 
     // for the MR diagram (multiple stars):
-    const unsigned Nstars = 1;
-    double MRarray[Nstars][3] = {0.};
-
     // define some global values:
-    double mu = 0.0;        // DM mass
+    double mu = 1.;        // DM mass
     double lambda = 0.0;    //self-interaction parameter
-    double R_fermi = 0.0; double M_total = 0.0; // fermionic radius of star and total mass of star
 
-    double rho_c = 2e-4; // central density
-    double phi_c = 0.;
+    double rho_c = 0.002; // central density
+    double phi_c = 0.02;
 
     // integrate ONE star and save all intermediate values into a txt file:
     // a, alpha, Phi, Psi, P(rho)
@@ -168,42 +170,9 @@ int main() {
 
     NSmodel m( EOS_poly, mu, lambda, 0.);
 
-    // uncomment this if you want to compute a full MR-diagram
-
-    std::cout << "start loop" << std::endl;
-
-    // create a simple MR-diagram:
-    for (unsigned i = 0; i < Nstars; ++i) {
-
-        double rho_start = (i+1)*rho_c;
-
-        // a, alpha, Phi, Psi, P(rho)
-        vector inits =  m.initial_conditions(0., rho_start, phi_c);
-        Shooting_integrator_nosave_intermediate(R_fermi, M_total, inits, &m);
-        //Shooting_integrator_nosave_intermediate(R_fermi, M_total, inits, myDD2, mu, lambda);
-
-        MRarray[i][0] = R_fermi;
-        MRarray[i][1] = M_total;
-        MRarray[i][2] = rho_start;
-
-       std::cout << i << std::endl;
-    }
-
-    std::cout << "end loop" << std::endl;
-
-    // print the results:
-    for (unsigned j = 0; j < Nstars; ++j) {
-        //std::cout << j << std::endl;
-        std::cout << "R = " << MRarray[j][0]*1.476625061 << " [km], M = " << MRarray[j][1] << " [M_sun]" << std::endl;
-    }
-
-    // save MR data in text file:
-    //save_data_MR(MRarray, Nstars, "MRtest_DD2eos.txt");
-
-
-    // print the results for now (later save them into a txt file).
-    //std::cout << "Star with rho_c = " << rho_c << ": radius = " << R_fermi << " [M], mass = " << M_total << " [M_sun?]" << std::endl;
-    //matplotlibcpp::show();
+    vector inits =  m.initial_conditions(0., rho_c, phi_c);
+    double omega_0 = 1., omega_1 =10.;
+    Bisection(&m, inits, omega_0, omega_1);
 
     /* see https://github.com/lava/matplotlib-cpp/issues/268 */
     matplotlibcpp::detail::_interpreter::kill();
