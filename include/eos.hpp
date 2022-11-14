7c49db75badaec734dcfc38401dbd9f4aca9c938
Niklas Becker <nbecker@itp.uni-frankfurt.de>
2022-11-14 14:11:14 +0100

    added comments and renamed variables for clarity in eos implementation

--- include/eos.hpp
+++ include/eos.hpp
@@ -12,22 +12,35 @@
 // reads in a EOS table in the constructor.
 // Then it serves as a look-up table for the EOS (p=p(rho,epsilon,...)) to use in the integrator.
 
-/* an abstract class to model what an equation of state should contain
+/* Equation of State
+ *  an abstract class to model what an equation of state should contain
+ *
+ *  min_P, min_rho describe minimal P, minimal rho where the EoS is valid
  * */
 class EquationOfState
 {
 public:
-    virtual double get_P_from_rho(const double rho_in, const double epsilon)  = 0;
+    /* This gives the pressure P depending on the matter density rho and internal energy epsilon */
+    virtual double get_P_from_rho(const double rho, const double epsilon)  = 0;
+
+    /* This gives the derivative dP/drho depending on the matter density rho and internal energy epsilon */
     virtual double dP_drho(const double rho, double epsilon) = 0;
 
+    /* This gives the matter density rho and internal energy epsilon depending on the pressure P */
 	virtual void callEOS(double& myrho, double& epsilon, const double P) = 0;
 
+    /* Functions giving minimal P and minimal rho for the EoS to be valid */
     virtual double min_P() = 0;
     virtual double min_rho() = 0;
 
 };
 
-/* a class modeling a Polytropic equation of state */
+/* PolytropicEoS
+ * a class modeling a Polytropic equation of state
+ * with the parameters Kappa, Gamma
+ * such that
+ *  P = Kappa * rho^{Gamma}
+ * */
 class PolytropicEoS : public EquationOfState
 {
 protected:
@@ -36,17 +49,26 @@ protected:
 public:
     PolytropicEoS(const double kappa=100., const double Gamma =2.) : kappa(kappa), Gamma(Gamma) {}
 
+    /* This gives the pressure P depending on the matter density rho. The internal energy density is ignored */
     double get_P_from_rho(const double rho_in, const double epsilon);
-	void callEOS(double& myrho, double& epsilon, const double P);
+    /* This gives the derivative dP/drho depending on the matter density rho. The internal energy density is ignored */
     double dP_drho(const double rho, double epsilon);
+    /* This gives the matter density rho and internal energy density depending on the pressure P */
+	void callEOS(double& myrho, double& epsilon, const double P);
 
+    /* Functions giving minimal P and minimal rho. For the polytrope both are 0. */
     double min_P();
     double min_rho();
 
 };
 
 
-/* a class modeling a Polytropic equation of state */
+/* CausalEoS
+ * a class modeling a causal equation of state
+ * with parameters eps_f, P_F,
+ * such that
+ *  P = P_f + rho(1+eps) - eps_f
+ * */
 class CausalEoS : public EquationOfState
 {
 protected:
@@ -55,35 +77,46 @@ protected:
 public:
     CausalEoS(const double eps_f, const double P_f =0.) : eps_f(eps_f), P_f(P_f) {}
 
+    /* This gives the pressure P depending on the matter density rho and internal energy epsilon */
     double get_P_from_rho(const double rho_in, const double epsilon);
+
+    /* This gives the derivative dP/drho depending on the matter density rho and internal energy epsilon */
     double dP_drho(const double rho, double epsilon);
+
+    /* This gives the matter density rho and internal energy epsilon depending on the pressure P */
 	void callEOS(double& myrho, double& epsilon, const double P);
 
+    /* Functions giving minimal P and minimal rho. For the causal EoS both are 0. */
     double min_P();
     double min_rho();
 };
 
-/* a class modeling a tabulated equation of state */
+/* EoStable
+ * a class representing a tabulated equation of state
+ * On construction, the tabulated EoS has to be loaded into the
+ *  vectors rho, Pres, e_tot,   the mass density, Pressure, and energy density respectively
+ * On call, the values are interpolated linearly from the table
+ * */
 class EoStable : public EquationOfState
 {
+protected:
+	std::vector<double> rho_table, P_table, e_table;
+
 public:
+    /* Constructor expects link to file TODO: add constructor with lists */
     EoStable(const std::string filename);
-	// call EOS lookup-table (including interpolation):
+
+    /* This gives the pressure P depending on the matter density rho and internal energy epsilon by linear interpolation of the table*/
 	void callEOS(double& myrho, double& epsilon, const double P);
+    /* This gives the derivative dP/drho depending on the matter density rho and internal energy epsilon by linear interpolation */
     double dP_drho(const double rho, double epsilon);
+    /* This gives the matter density rho and internal energy epsilon depending on the pressure P by linear interpolation*/
     double get_P_from_rho(const double rho_in, const double epsilon);
 
+    /* Functions giving minimal P and minimal rho. These depend on the lowest values in the tables */
     double min_P();
     double min_rho();
 
-private:
-	// vectors to hold the tabulated EOS values:
-	std::vector<double> rho;	// restmass density
-	std::vector<double> Pres;	// pressure
-	std::vector<double> e_tot;	// total energy density e:=rho(1+epsilon)
-	// helper variables:
-	unsigned current_index;		// saves position in look up table
-	std::string eos_table_name; // name of EOS/table to use (filename of txt file)
 };
 
 #endif
--- src/eos.cpp
+++ src/eos.cpp
@@ -1,21 +1,22 @@
 #include "eos.hpp"
 
-/* PolytropicEoS */
-double PolytropicEoS::get_P_from_rho(const double rho_in, const double epsilon) {
-
-	return this->kappa*std::pow(rho_in, this->Gamma);	// p = k*rho^gamma
+/******************
+ * PolytropicEoS *
+ ******************/
+double PolytropicEoS::get_P_from_rho(const double rho, const double epsilon) {
+	return this->kappa*std::pow(rho, this->Gamma);
 }
 
-double PolytropicEoS::dP_drho(const double rho_in, const double epsilon) {
-
-	return this->kappa*this->Gamma*std::pow(rho_in, this->Gamma-1.);
+double PolytropicEoS::dP_drho(const double rho, const double epsilon) {
+	return this->kappa*this->Gamma*std::pow(rho, this->Gamma-1.);
 }
-
-void PolytropicEoS::callEOS(double& myrho, double& epsilon, const double P) {
-	// update restmass density (rho) and specific energy (epsilon) according to polytropic EOS
-    	myrho = std::pow(P / this->kappa, 1./this->Gamma);
-    	epsilon = this->kappa*std::pow(myrho, this->Gamma - 1.) / (this->Gamma - 1.);
-		return;
+/* This expects as input P and will give rho, epsilon as output through reference
+ * according to the polytropic EoS
+ * TODO : update restmass density (rho) and specific energy (epsilon) according to polytropic EOS */
+void PolytropicEoS::callEOS(double& rho, double& epsilon, const double P) {
+    rho = std::pow(P / this->kappa, 1./this->Gamma);
+    epsilon = this->kappa*std::pow(rho, this->Gamma - 1.) / (this->Gamma - 1.);
+    return;
 }
 
 double PolytropicEoS::min_P() {
@@ -26,20 +27,24 @@ double PolytropicEoS::min_rho() {
 }
 
 
-/* CausalEoS */
-double CausalEoS::get_P_from_rho(const double rho_in, const double epsilon) {
+/******************
+ *  CausalEoS *
+ ******************/
+double CausalEoS::get_P_from_rho(const double rho, const double epsilon) {
 
-	return this->P_f + rho_in*(1. + epsilon) - this->eps_f;   // p = P_f + eps - eps_f
+	return this->P_f + rho*(1. + epsilon) - this->eps_f;   // p = P_f + eps - eps_f
 }
 
-double CausalEoS::dP_drho(const double rho_in, const double epsilon) {
+double CausalEoS::dP_drho(const double rho, const double epsilon) {
 
 	return (1. + epsilon);   // p = P_f + eps - eps_f
 }
 
-void CausalEoS::callEOS(double& myrho, double& epsilon, const double P) {
-	// update restmass density (rho) and specific energy (epsilon) according to causal EOS
-        myrho = P - this->P_f + this->eps_f;
+/* This expects as input P and will give rho, epsilon as output through reference
+ * according to the causal EoS
+ * TODO : update restmass density (rho) and specific energy (epsilon) according to causal EOS */
+void CausalEoS::callEOS(double& rho, double& epsilon, const double P) {
+        rho = P - this->P_f + this->eps_f;
         epsilon = 0.;
 }
 
@@ -50,125 +55,135 @@ double CausalEoS::min_rho() {
     return 0.;
 }
 
-/* EoStable */
+/******************
+ *  EoStable *
+ ******************/
+/* This constructor expects a filename to a table. This table has to have the following structure
+ *
+ * | restmass density rho [1/fm^3] |  (skipped)  | energy density[MeV/fm^3]  |  pressure[MeV/fm^3] |
+ *
+ * Lines starting with # are ignored
+ *
+ * If the file cannot be found, an exception is thrown
+ * TODO: Outsource file reading into function and allow different constructors (one with std::vectors right away)
+ */
 EoStable::EoStable(const std::string filename) {
-    // load in the tabulated EOS data and convert it to code units directly
 
-    eos_table_name = filename;
     const double MeV_fm3_to_codeunits =	2.886376934e-6; // unit conversion from Mev/fm^3 to code units M_s*c^2/ (G*M_s/c^2)^3
     const double neutron_mass = 939.565379;	 // nuclear density in MeV
 
-    std::ifstream infile;	// declare input file stream
-    infile.open(eos_table_name);
-
+    std::ifstream infile;
+    infile.open(filename);
     if(!infile.is_open())
-        throw std::runtime_error("File not open");
+        throw std::runtime_error("File " + filename + " not open");
+
     std::string line;
-
     while (std::getline(infile, line)) {
-
         double temp;
         std::stringstream ss(line);
 
 		// ignore lines with '#' in it (to make comment in the table)
 		if (line.find('#') != std::string::npos)
-    		continue; // '#' found, therefore we skip this line
-
-		// ---------------------------------------
-		// read in the EOS format from the CompOSE code. Format is:
-		// n_b[1/fm^3]  Y_e[dimensionless]  energy density[MeV/fm^3]  p[MeV/fm^3]
+    		continue;
 
         ss >> temp; // column 1 -> restmass density
-        rho.push_back(temp*MeV_fm3_to_codeunits*neutron_mass);	// write the 1nd column -> restmass density and convert to code units
+        rho_table.push_back(temp*MeV_fm3_to_codeunits*neutron_mass);	// write the 1nd column -> restmass density and convert to code units
 
         ss >> temp; // column 2 -> electron (lepton) fraction (we don't need it, skip this column)
 		ss >> temp; // column 3 -> energy density
-        e_tot.push_back(temp*MeV_fm3_to_codeunits);	// write the 3nd column -> energy density e=rho*(1+epsilon) and convert to code units
+        e_table.push_back(temp*MeV_fm3_to_codeunits);	// write the 3nd column -> energy density e=rho*(1+epsilon) and convert to code units
 
         ss >> temp; // column 4 -> pressure
-        Pres.push_back(temp*MeV_fm3_to_codeunits);	// write the 4nd column -> pressure and convert to code units
-
+        P_table.push_back(temp*MeV_fm3_to_codeunits);	// write the 4nd column -> pressure and convert to code units
     }
-    infile.close();	// close file after reading from it
-
+    infile.close();
 }
 
-// call EOS to obtain rho and e from input P:
-void EoStable::callEOS(double& myrho, double& epsilon, const double P) {
+/* This expects as input P and will give rho, epsilon as output through reference
+ * according to the tabulated EoS
+ * If we are below or above the pressures in Pres, 0. is returned
+ *  otherwise simple linear interpolation is used to obtain rho, epsilon */
+void EoStable::callEOS(double& rho, double& epsilon, const double P) {
 
 	// use P to scan the EOS table (iterate from the top first because we start at high pressures):
-	unsigned int table_len = Pres.size();
+	unsigned int table_len = P_table.size();
 	double e_tot_tmp = 0.0;
 
 
-	if (P < Pres[0]) { // we are outside the validity range of the table. Return zero
-		myrho = 0.;
+	if (P < P_table[0]) { // we are outside the validity range of the table. Return zero
+		rho = 0.;
 		epsilon = 0.;
 		return;
 	}
 
 	// search the table from the smaller values on first
 	for (unsigned int i = 1; i<table_len; i++) {
-		if (Pres[i] > P) {
+		if (P_table[i] > P) {
 			// the correct value is between the i-1th index and the ith index:
 			// interpolate linearily between them:
-			myrho = rho[i-1] + (rho[i] - rho[i-1]) / (Pres[i] - Pres[i-1]) * (P - Pres[i-1]);
-			e_tot_tmp = e_tot[i-1] + (e_tot[i] - e_tot[i-1]) / (Pres[i] - Pres[i-1]) * (P - Pres[i-1]);
-			epsilon = e_tot_tmp/myrho - 1.0;	// re-arrange to get epsilon. e=rho*(1+epsilon)
+			rho = rho_table[i-1] + (rho_table[i] - rho_table[i-1]) / (P_table[i] - P_table[i-1]) * (P - P_table[i-1]);
+			e_tot_tmp = e_table[i-1] + (e_table[i] - e_table[i-1]) / (P_table[i] - P_table[i-1]) * (P - P_table[i-1]);
+			epsilon = e_tot_tmp/rho - 1.0;	// re-arrange to get epsilon. e=rho*(1+epsilon)
 			return;
 		}
 	}
 	// return 0. outside the validity
-    myrho = 0.;
+    rho = 0.;
     epsilon = 0.;
 }
 
-// obtain P from a rho input
-double EoStable::get_P_from_rho(const double rho_in, const double epsilon) {
-	unsigned int table_len = rho.size();
+/* This expects as input rho and returns P (epsilon is ignored)
+ * according to the tabulated EoS
+ * If we are below or above the densities in rho, 0. is returned
+ *  otherwise simple linear interpolation is used to obtain P from rho */
+double EoStable::get_P_from_rho(const double rho, const double epsilon) {
+	unsigned int table_len = rho_table.size();
 
     // if we are below the table return 0.
-    if (rho_in < rho[0])
+    if (rho < rho_table[0])
         return 0.;
 
 	// search the table for the correct value
 	for (unsigned int i = 1; i<table_len; i++) {
-		if (rho[i] > rho_in) {
+		if (rho_table[i] > rho) {
 			// the correct value is between the ith index and the i+1th index:
 			// interpolate linearily between them:
-			return Pres[i-1] + (Pres[i] - Pres[i-1]) / (rho[i] - rho[i-1]) * (rho_in - rho[i-1]);
+			return P_table[i-1] + (P_table[i] - P_table[i-1]) / (rho_table[i] - rho_table[i-1]) * (rho - rho_table[i-1]);
 		}
 	}
     // if no value was found return 0.
 	return 0.;
 }
 
-// obtain dP/drho from a rho input
-double EoStable::dP_drho(const double rho_in, const double epsilon) {
+/* This expects as input rho and returns dP/drho (epsilon is ignored)
+ * according to the tabulated EoS
+ * If we are below or above the densities in rho, 0. is returned
+ *  otherwise simple linear interpolation is used to obtain P from rho */
+double EoStable::dP_drho(const double rho, const double epsilon) {
 	// search the table for the correct value:
-	unsigned int table_len = rho.size();
+	unsigned int table_len = rho_table.size();
 
-    //assert(rho_in < rho[table_len-2]); // out of range will return 0.
-    if(rho_in < rho[1])
+    //assert(rho < rho_table[table_len-2]); // out of range will return 0.
+    if(rho < rho_table[1])
         return 0.;
 
 	for (unsigned int i = 2; i<table_len; i++) {
 		// scan for the first matching rho
-		if (rho[i] > rho_in) {
+		if (rho_table[i] > rho) {
             // estimate derivative at point i-1 and i
-            double dP1 = (Pres[i]-Pres[i-1])/(rho[i]-rho[i-1])/2. + (Pres[i-1] - Pres[i-2])/(rho[i-1]-rho[i-2])/2.;
-            double dP2 = (Pres[i+1]-Pres[i])/(rho[i+1]-rho[i])/2. + (Pres[i] - Pres[i-1])/(rho[i]-rho[i-1])/2.;
-            return dP1 + (dP2-dP1)/(rho[i]- rho[i-1]) * (rho_in - rho[i-1]);
+            double dP1 = (P_table[i]-P_table[i-1])/(rho_table[i]-rho_table[i-1])/2. + (P_table[i-1] - P_table[i-2])/(rho_table[i-1]-rho_table[i-2])/2.;
+            double dP2 = (P_table[i+1]-P_table[i])/(rho_table[i+1]-rho_table[i])/2. + (P_table[i] - P_table[i-1])/(rho_table[i]-rho_table[i-1])/2.;
+            return dP1 + (dP2-dP1)/(rho_table[i]- rho_table[i-1]) * (rho - rho_table[i-1]);
 		}
 	}
 	return 0.;
 }
 
 double EoStable::min_P() {
-    return this->Pres.at(0);
+    return this->P_table.at(0);
 }
 double EoStable::min_rho() {
-    return this->rho.at(0);
+    return this->rho_table.at(0);
 }
 
 /*
