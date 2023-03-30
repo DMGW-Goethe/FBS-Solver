34633919e8bc1ee5d56bcf600d26e9524a4af7a4
xcex <cedric.jockel@online.de>
2023-03-30 18:08:23 +0200

    fixed dublicated function leading to compilation error

--- main.cpp
+++ main.cpp
@@ -154,59 +154,6 @@ void test_effectiveEOS_pure_boson_star() {
 }
 
 
-// compute here e.g. a few configurations with the full system and with the effective EOS for different lambda, to check if they produce the same results.
-void test_effectiveEOS_pure_boson_star() {
-
-	double mu = 0.5;
-	double Lambda_int = 10.;
-	double lambda = Lambda_int*8*M_PI*mu*mu;
-
-	// create the phi_c-rho_c-grid:
-	const unsigned NstarsPhi = 10;
-	const unsigned NstarsRho = 1;
-	std::vector<double> rho_c_grid(NstarsRho, 0.0), phi_c_grid(NstarsPhi, 0.0);
-
-	double rho_cmin = 1e-10;	// = [saturation_density] / 0.15 * 2.886376934e-6 * 939.565379 -> includig conversion factors from saturation density to code units
-	double rho_cmax = 5e-3;
-	double phi_cmin = 1e-5;
-	double phi_cmax = 0.055;
-	
-	fillValuesPowerLaw(phi_cmin, phi_cmax, phi_c_grid, 1);
-    fillValuesPowerLaw(rho_cmin, rho_cmax, rho_c_grid, 1);
-
-	// declare different EOS types:
-    auto EOS_DD2 = std::make_shared<EoStable>("EOS_tables/eos_HS_DD2_with_electrons.beta");
-	/*auto EOS_APR = std::make_shared<EoStable>("EOS_tables/eos_SRO_APR_SNA_version.beta");
-	auto EOS_KDE0v1 = std::make_shared<EoStable>("EOS_tables/eos_SRO_KDE0v1_SNA_version.beta");
-	auto EOS_LNS = std::make_shared<EoStable>("EOS_tables/eos_SRO_LNS_SNA_version.beta");
-	auto EOS_FSG = std::make_shared<EoStable>("EOS_tables/eos_HS_FSG_with_electrons.beta");*/
-	//auto EOS_DD2 = std::make_shared<PolytropicEoS>();
-	auto myEffectiveEOS = std::make_shared<EffectiveBosonicEoS>(mu, lambda);
-
-
-	// compute full self-consistent system:
-	// setup to compute a full NS configuration, including tidal deformability:
-	std::vector<FermionBosonStar> MRphi_curve; std::vector<FermionBosonStarTLN> MRphi_tln_curve;
-	// calc the unperturbed equilibrium solutions:
-    calc_rhophi_curves(mu, lambda, EOS_DD2, rho_c_grid, phi_c_grid, MRphi_curve, 2);
-	// calc the perturbed solutions to get the tidal love number:
-	calc_MRphik2_curve(MRphi_curve, MRphi_tln_curve, 2); // compute the perturbed solutions for TLN
-	// save the results in a txt file:
-	std::string plotname = "pureBS/1paperplot-TLN-line_pureBS_fullsys-mu_" + std::to_string(mu) + "_Lambdaint_" + std::to_string(Lambda_int);
-	//plotname = "tidal_pureEOS_KDE0v1_fullsys";
-	//write_MRphi_curve<FermionBosonStar>(MRphi_curve, "plots/" + plotname + ".txt");
-	write_MRphi_curve<FermionBosonStarTLN>(MRphi_tln_curve, "plots/" + plotname + ".txt");
-
-	// compute effective two-fluid model:
-	std::vector<TwoFluidFBS> twofluid_MRphi_curve;
-	calc_twofluidFBS_curves(EOS_DD2, myEffectiveEOS, rho_c_grid, phi_c_grid, twofluid_MRphi_curve, mu, lambda);	// use the effective EOS
-	plotname = "pureBS/1paperplot-TLN-line_pureBS_effsys-mu_" + std::to_string(mu) + "_Lambdaint_" + std::to_string(Lambda_int);
-	//plotname = "tidal_pureEOS_APR_twofluid";
-	//plotname = "rhophi-diagram_DD2_twofluid_" + std::to_string(mu) + "_" + std::to_string(lambda);
-	write_MRphi_curve<TwoFluidFBS>(twofluid_MRphi_curve, "plots/" + plotname + ".txt");
-}
-
-
 int create_MR_curve() {
 
     const unsigned Nstars = 30;     // number of stars in MR curve of constant Phi_c
